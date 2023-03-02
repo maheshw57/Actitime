2b041b469af5f11e1d0353c528e8581f833a2d07
lenova <lenova@DESKTOP-KQNCDJV>
2023-03-03 00:33:50 +0530

    Commit3

--- src/com/actiTime/generic/BaseClass.java
+++ src/com/actiTime/generic/BaseClass.java
@@ -5,25 +5,35 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
 import com.actiTime.pom.HomePage;
 import com.actiTime.pom.LoginPage;
 
 public class BaseClass {
 	static {
 		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","./driver/geckodriver.exe");
 	}
 	
-	public static WebDriver driver;
+	public  WebDriver driver;
 	public FileLib f=new FileLib();
+	 @Parameters("browser")
 	@BeforeClass
-	public void openBrowser() {
+	public void openBrowser(String browser) {
 		Reporter.log("openBrowser",true);
+    	if (browser.equals("chrome")){
 		driver=new ChromeDriver();
+		}
+		else if(browser.equals("firefox")){
+			driver=new FirefoxDriver();
+		 }
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
